a7f8776ae4be97d0cccb7021377b759aa36af085
StriderX1X <rishabhkalathiya987@gmail.com>
2020-02-15 14:47:48 +0530

    error stmt added

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,12 @@
 console.log("client side js file is loaded!");
 
 const weatherForm = document.querySelector("form");
-const search = document.querySelector("input");
+const search_field = document.querySelector("input");
 const board = document.querySelector("h4");
 
 weatherForm.addEventListener("submit",(e)=>{
     e.preventDefault();
-    const location = search.value;
+    const location = search_field.value;
     console.log(location);
     fetch("/weather?address="+location).then((response) => {
     response.json().then((data) => {
